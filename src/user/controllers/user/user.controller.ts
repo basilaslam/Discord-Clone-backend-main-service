864e8df0dac3de6f8f33b678bedd1aefbba21f13
Mukhthar Azeez <mukhtharazeez28@gmail.com>
2023-01-09 09:44:44 +0530

    server got back

--- src/user/controllers/user/user.controller.ts
+++ src/user/controllers/user/user.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { CreateUserWithProvidersDto } from './../../dto/createUserWithProviders.dto';
 import { CreateUserDto } from 'src/user/dto/createUser.dto';
 import { UserService } from './../../services/user/user.service';
